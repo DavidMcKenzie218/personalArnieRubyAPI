3be881f88d6305b5d1710c54e5ac4fb5290a2710
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2017-06-28 20:26:21 +0100

    Added the test database that will be used for testing

---
+++ db/testDb.sql
@@ -0,0 +1,13 @@
+DROP TABLE testData;
+
+CREATE TABLE testData(
+  id serial4 primary key,
+  spoken_word varchar(255),
+  response varchar(255),
+  catagory varchar(255)
+);
+
+INSERT INTO testData (spoken_word, response, catagory) VALUES ('hello', 'soundcloud.com/hello_there', 'greeting');
+INSERT INTO testData (spoken_word, response, catagory) VALUES ('goodbye', 'soundcloud.com/goodbye_for_now', 'farewell');
+INSERT INTO testData (spoken_word, response, catagory) VALUES ('bye', 'soundcloud.com/bye_sucka', 'farewell' );
+INSERT INTO testData (spoken_word, response, catagory) VALUES ('bye', 'soundcloud.com/leave_now', "farewell");
